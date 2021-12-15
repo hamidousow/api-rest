e5f261f08b1b6b06d322deef090bad13b2fb9464
Hamidou.S <86967645+hamidousow@users.noreply.github.com>
2021-12-15 12:21:17 +0100

    schema and routs created

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const Product = require('./models/Product')
 
 mongoose.connect('mongodb+srv://user-test:motdepasse@cluster0.6fhsl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
         {
@@ -18,13 +19,42 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use('/api/stuff' , (req, res, next) => {
-    const stuff = [
-        {
-            message: "c'est bon ca fonctionne"
-        }
-    ];
-    res.status(200).json(stuff);
+app.post('/api/products' , (req, res, next) => {
+    delete req.body._id;
+    const product = new Product ({
+        ...req.body
+    })
+    thing.save()
+    .then(() => {
+        res.status(201).json({message: 'produit ajouté'})
+    })
+    .catch(error => {
+        res.status(201).json({error})
+    });
+})
+
+app.get('/api/products/:id', (req, res, next) => {
+    Product.findOne({ _id: req.params.id})
+    .then(product => res.status(200).json(product))
+    .catch(error => res.status(404).json({ error }))
+})
+
+app.use('/api/products', (req, res, next) => {
+    Product.find()
+    .then(products => res.status(200).json(products))
+    .catch(error => res.status(400).json({ error }))
+})
+
+app.put('/api/products/:id', (req, res, next) => {
+    Product.updateOne({ _id: req.params.id}, { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({message: 'objet bien modifé'}))
+    .catch(error => res.status(400).json({ error }))
+})
+
+app.delete('/api/products/:id', (req, res, next) => {
+    Thing.deleteOne({_id: req.params.id})
+    .then(() => res.status(200).json({ messge: "iobjet supprimé"}))
+    .catch(error => res.status(400).json({ error }))
 })
 
 module.exports = app;
