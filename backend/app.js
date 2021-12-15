0e4cbd83847c289be69aa8036c57f38bf66399b0
Hamidou.S <86967645+hamidousow@users.noreply.github.com>
2021-12-15 09:36:46 +0100

    cors authorizations

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,20 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    res.json({ message: 'votre requête a bien été reçue'})
+app.use('/api/stuff', (req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+})
+
+app.use('/api/stuff' , (req, res, next) => {
+    const stuff = [
+        {
+            message: "c'est bon ca fonctionne"
+        }
+    ]
+    res.status(200).json(stuff)
 })
 
 module.exports = app;
